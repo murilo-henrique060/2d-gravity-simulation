256d4a81a16be859c68a42fcb891db7294c67a8c
Murilo Henrique <77072462+murilo-henrique060@users.noreply.github.com>
2022-07-26 19:49:21 -0300

    pause key and focus object

--- main/body.py
+++ main/body.py
@@ -19,7 +19,10 @@ class Body(Circle):
         self.acceleration = np.float64(np.array(initial_acceleration))
         self.mass = mass * 10 ** 6
 
-        self.direction = Line(self.plane, (255, 255, 255), self.center, self.center + self.speed * 10)
+        self.direction = Line(self.plane, (255, 255, 255), self.center, self.center + self.speed * 100)
+
+    def point_in_circle(self, point: tuple | list):
+        return math.dist(self.center, point) <= self.radius
 
     def update(self):
         self.center[0] += self.speed[0]
@@ -29,7 +32,7 @@ class Body(Circle):
         self.speed[1] += self.acceleration[1]
 
         self.direction.start = self.center
-        self.direction.end = self.center + self.speed * 10
+        self.direction.end = self.center + self.speed * 100
 
     def gravity(self, other):
         distance = math.dist(self.center, other.center)
@@ -47,26 +50,47 @@ class Group():
 
         self.add(*bodies)
 
+        self.focus = None
+
+    def point_in_group(self, point: tuple | list):
+        for body in self.group:
+            if body.point_in_circle(point):
+                return body
+        return None
+
     def add(self, *bodies: Body):
         for body in bodies:
             self.group.append(body)
 
     def update(self):
         if self.simulation:
-            for body in self.group:
-                for other in self.group:
-                    if body != other:
-                        body.gravity(other)
+            for i in range(1, 10):
+                for body in self.group:
+                    for other in self.group:
+                        if body != other:
+                            body.gravity(other)
 
-            for body in self.group:
-                body.update()
+                for body in self.group:
+                    body.update()
+
+            if self.focus:
+                center = [self.focus.plane.size[0] // 2, self.focus.plane.size[1] // 2]
+                focus_center = self.focus.plane.plane_to_screen(self.focus.center)
+                diff = [
+                    focus_center[0] - center[0],
+                    focus_center[1] - center[1]
+                ]
+
+                self.focus.plane.offset[0] -= diff[0] / self.focus.plane.scale_factor
+                self.focus.plane.offset[1] -= diff[1] / self.focus.plane.scale_factor
+
 
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
 plane = CartesianPlane(screen)
 
 body = Body(plane, (255, 0, 0), (0, 0), 50, [0, 0], [0, 0], 10 ** 4)
-body2 = Body(plane, (0, 0, 255), (100, 100), 10, [0, 0], [0, 0], 1)
+body2 = Body(plane, (0, 0, 255), (0, 100), 10, [0.081, 0], [0, 0], 1)
 body_group = Group(body, body2)
 clock = pygame.time.Clock()
 
@@ -76,20 +100,20 @@ while True:
             pygame.quit()
             quit()
 
-        if event.type == KEYUP and event.key == K_ESCAPE:
+        if event.type == KEYUP and event.key == K_SPACE:
             body_group.simulation = not body_group.simulation
+
+        if event.type == MOUSEBUTTONDOWN and event.button == 1:
+            body_group.focus = body_group.point_in_group(plane.screen_to_plane(pygame.mouse.get_pos()))
 
         plane.event_handling(event)
 
     plane.debug(
         fps=f'{clock.get_fps():.1f}',
-        body_2_angle=f'{math.degrees(math.atan2(body2.direction.end[1] - body2.direction.start[1], body2.direction.end[0] - body2.direction.start[0])):.2f}',
-        body_2_center=f'{body2.center}',
-        body_2_speed=f'{body2.speed[0]*100000:.2f} {body2.speed[1]*100000:.2f}',
-        body_2_acceleration=f'{body2.acceleration[0]*100000:.2f} {body2.acceleration[1]*100000:.2f}',
+        state='running' if body_group.simulation else 'paused',
     )
 
-    clock.tick()
+    clock.tick(120)
     plane.update()
     body_group.update()
     draw.line(plane, (255, 255, 255), body.center, body2.center)
